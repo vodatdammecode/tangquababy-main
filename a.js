1cd01ab9f4971d780e0a3efb18ff51d1c64dce4f
Võ Đạt  Đang Học Code <166628450+vodatdammecode@users.noreply.github.com>
2024-04-17 19:55:02 +0700

    Update a.js

--- a.js
+++ a.js
@@ -22,7 +22,7 @@ function initengahan(){
 
   function tombol(){wallpaper.style="transform: scale(1);";Tombol.style="opacity:1;transform: scale(1);";fungsi=1}
   document.getElementById("By").onclick = function() {if(fungsi==1){pertanyaan();} if(fungsi==2){menuju();}}
-  async function menuju(){await swals.fire('Uci!', 'Nhớ liên hệ anhh nhaaa bấy biii , anhh có này tặng choo bbi nàa!', 'success');window.location = "https://cuongnobro.github.io/Baby/";}
+  async function menuju(){await swals.fire('Uci!', 'Nhớ liên hệ tớ nhaaa cậu ơi , tớ có này tặng choo cậu nàa!', 'success');window.location = "https://cuongnobro.github.io/Baby/";}
   
   vketik1=kalimat.innerHTML;kalimat.innerHTML = "";
   function mulaiketik1(){
